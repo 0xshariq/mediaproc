e721143eda7c222eaf032c935a06d7586d31394e
Sharique Chaudhary <khanshariq92213@gmail.com>
2026-01-23 11:28:06 +0000

    refactor: add effectNamespaces export and fix normalizeColor export formatting

--- core/src/utils/index.ts
+++ core/src/utils/index.ts
@@ -1,3 +1,4 @@
 // Utils
 export * from './constants/supportedExtensions.js';
-export * from './normalizeColor.js'
+export * from './normalizeColor.js';
+export * from './constants/effectNamespaces.js'
